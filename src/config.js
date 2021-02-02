c2295abcf6de16f5451aea448f31d4f118790a77
Haayhur <47255529+Haayhur@users.noreply.github.com>
2021-02-02 15:31:53 +0100

    update

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb+srv://Ayodele:Ayodele123@cluster0.jasxe.mongodb.net/quiz?retryWrites=true&w=majority'
+}
--- src/server.js
+++ src/server.js
@@ -10,11 +10,20 @@ app.use(cors()) // We're telling express to use CORS
 app.use(express.json()) // we need to tell server to use json as well
 app.use(routes) // tells the server to use the routes in routes.js
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('database connected'))
 
-app.listen(process.env.PORT, () => {
+var config = require('./config');
+const url = config.mongoUrl;
+const connect = mongoose.connect(url,{useNewUrlParser: true,useUnifiedTopology: true});
+
+connect.then((db) => {
+  console.log('Connected to server');
+}, (err) => {console.log(err);});
+
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// const db = mongoose.connection
+// db.on('error', (error) => console.error(error))
+// db.once('open', () => console.log('database connected'))
+
+app.listen(5000, () => {
     console.log("The api is running...")
 })
