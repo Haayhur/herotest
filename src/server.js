56ca632f13e6b566c4c07a2f705f208978193439
Haayhur <47255529+Haayhur@users.noreply.github.com>
2021-02-02 16:11:46 +0100

    new

--- src/server.js
+++ src/server.js
@@ -22,8 +22,10 @@ connect.then((db) => {
 // mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 // const db = mongoose.connection
 // db.on('error', (error) => console.error(error))
-// db.once('open', () => console.log('database connected'))
+// db.once('open', () => console.log('database connected'))\
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`The api is running... ${port}!`));
 
-app.listen(80, () => {
-    console.log("The api is running...")
-})
+// app.listen(80, () => {
+//     console.log("The api is running...")
+// })
